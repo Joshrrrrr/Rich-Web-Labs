193cef8100fd86c4bbb28ada7317c7232185ffa9
C19303023 Josh Reilly <C19303023@mydit.ie>
2022-10-17 11:57:20 +0100

    problem-1 commit

--- lab2/problem-2/index.js
+++ lab2/problem-2/index.js
@@ -2,32 +2,23 @@
 showNotes();
 
 // If user adds a note add to the localStorage
-var color = "white";
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
-  let addTxt = document.getElementById("addTxt");
+  let name = document.getElementById("name");
+  let mobile = document.getElementById("mobile");
+  let email = document.getElementById("email");
   let notes = localStorage.getItem("notes");
   if (notes == null) notesObj = [];
   else notesObj = JSON.parse(notes);
-
-  notesObj.push(addTxt.value);
+  let contact = [name.value,mobile.value,email.value]; 
+  notesObj.push(contact);
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
+  name.value = "";
+  mobile.value = "";
+  email.value = "";
 
   showNotes();
 });
-function blue() {
-  color = document.getElementById("blue").value;
-  showNotes();
-}
-function red() {
-  color = document.getElementById("red").value;
-  showNotes();
-}
-function green() {
-  color = document.getElementById("green").value;
-  showNotes();
-}
 // show elements from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
@@ -39,8 +30,7 @@ function showNotes() {
   // use internal css for the noteCards so changing the color and text is easy
   notesObj.forEach(function (element, index) {
     html += `<div class="noteCard"
-			style="width:18rem;margin-right: 50px;margin-bottom: 50px;
-			background-color:${color}">
+			style="width:18rem;margin-right: 50px;margin-bottom: 50px">
 				<div class="card-body">
 					<p class="card-text">${element}</p>
 				<button id="${index}" onclick=
@@ -54,7 +44,7 @@ function showNotes() {
   let notesElm = document.getElementById("notes");
   // check if there are any notes in notesObj
   if (notesObj.length != 0) notesElm.innerHTML = html;
-  else notesElm.innerHTML = "Nothing to show";
+  else notesElm.innerHTML = "No contacts";
 }
 
 // delete a note
