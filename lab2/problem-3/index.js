16fe2fbf9f5140cf35e142447321e808d3280950
JoshReilly <joshreilly2000@gmail.com>
2022-10-24 16:40:02 +0100

    final commit

--- lab2/problem-3/index.js
+++ lab2/problem-3/index.js
@@ -1,96 +1,73 @@
-// Get the GitHub username input form
-const gitHubForm = document.getElementById('gitHubForm');
+// retrieve form
+const gitHubForm = document.getElementById("gitHubForm");
+gitHubForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Get username input
+  let usernameInput = document.getElementById("usernameInput");
+  let gitHubUsername = usernameInput.value;
+  requestUserRepos(gitHubUsername);
+});
 
-// Listen for submissions on GitHub username input form
-gitHubForm.addEventListener('submit', (e) => {
-    
-    // Prevent default form submission action
-    e.preventDefault();
+function requestUserRepos(username) {
+  document.getElementById("userRepos").style.visibility = "visible";
+  // Create 2 XMLHttpRequest objects
+  const xhr = new XMLHttpRequest();
+  const xhr2 = new XMLHttpRequest();
+  let email = "";
+  let location = "";
+  let gist = "";
+  // GitHub endpoint
+  const url = `https://api.github.com/users/${username}/repos`;
+  const user = `https://api.github.com/users/${username}`;
+  // Providing 3 arguments (GET/POST, The URL, Async True/False)
+  xhr.open("GET", user, true);
+  // When request received from /user request
+  xhr.onload = function () {
+    const data = JSON.parse(this.response);
+    console.log(data);
+    email = `${data.email}`;
+    location = `${data.location}`;
+    gist = `${data.public_gists}`;
+  };
+  xhr.send();
+  xhr2.open("GET", url, true);
 
-    // Get the GitHub username input field on the DOM
-    let usernameInput = document.getElementById('usernameInput');
-
-    // Get the value of the GitHub username input field
-    let gitHubUsername = usernameInput.value;          
-
-    // Run GitHub API function, passing in the GitHub username
-    requestUserRepos(gitHubUsername);
-
-})
-
-
-function requestUserRepos(username){
-    
-    // Create new XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
-    const xhr2 = new XMLHttpRequest();
-    let email = "";
-    let location= "";
-    let gist= "";
-    // GitHub endpoint, dynamically passing in specified username
-    const url = `https://api.github.com/users/${username}/repos`;
-    const user = `https://api.github.com/users/${username}`;
-    // Open a new connection, using a GET request via URL endpoint
-    // Providing 3 arguments (GET/POST, The URL, Async True/False)
-    xhr.open('GET', user, true);
-    // When request is received
-    // Process it here
-    xhr.onload = function () {
-        const data = JSON.parse(this.response);
-        console.log(data);
-        email=(`${data.email}`);
-        location=(`${data.location}`);
-        gist=(`${data.public_gists}`);
-    }
-    xhr.send();
-    xhr2.open('GET', url, true);
-    
-    // When request is received
-    // Process it here
-    xhr2.onload = function () {
-    
-        // Parse API data into JSON
-        const data = JSON.parse(this.response);
-        
-        // Loop over each object in data array
-        for (let i in data) {
-            // Get the ul with id of of userRepos
-            let ul = document.getElementById('userRepos');
-    
-            // Create variable that will create li's to be added to ul
-            let li = document.createElement('li');
-            
-            // Add Bootstrap list item class to each li
-            li.classList.add('list-group-item')
-        
-            // Create the html markup for each li
-            li.innerHTML = (`
-                <div style="float:left;width:50%">
+  // When request received from /user/repos request
+  xhr2.onload = function () {
+    // parse API data into JSON
+    const data = JSON.parse(this.response);
+    let li2 = document.getElementById("userProf");
+    li2.innerHTML = `
+                <div>
                 <h2>User Profile</h2>
-                <table>
-                <tr><td><img src=${data[i].owner.avatar_url} style="width:100px;height:100px"/></td></tr>
-                <tr><td><p><strong>Name ${data[i].owner.login}</strong></p></td></tr>
-                <tr><td><p><strong>Username ${data[i].owner.login}</strong></p></td></tr>
-                <tr><td><p><strong>Email ${email}</strong></p></td></tr>
-                <tr><td><p><strong>Location ${location}</strong></p></td></tr>
-                <tr><td><p><strong>Number of Gists ${gist}</strong></p></td></tr>
-                </table>
+                <img src=${data[0].owner.avatar_url} style="width:100px;height:100px"/>
+                <p><strong>Name ${data[0].owner.login}</strong></p>
+                <p><strong>Username ${data[0].owner.login}</strong></p>
+                <p><strong>Email ${email}</strong></p>
+                <p><strong>Location ${location}</strong></p>
+                <p><strong>Number of Gists ${gist}</strong></p
                 </div>
-                <div style="float:right;width:50%">
-                <h2>User Repos</h2>
+                `;
+    // Loop over data array
+    for (let i in data) {
+      // Get the ul with id of of userRepos
+      let ul = document.getElementById("userRepos");
+      // lis to be added to ul
+      let li = document.createElement("li");
+      // Add Bootstrap list item class
+      li.classList.add("list-group-item");
+      li.innerHTML = `
+                <div>
                 <p><strong>Name ${data[i].name}</strong></p>
                 <p><strong>Description ${data[i].description}</strong></p>
                 </div>
-            `);
-            
-            // Append each li to the ul
-            ul.appendChild(li);
-        
-        }
+            `;
 
+      // Append each li to the ul
+      ul.appendChild(li);
     }
-    
-    // Send the request to the server
-    xhr2.send();
-    
+  };
+
+  // Send the request
+  xhr2.send();
 }
