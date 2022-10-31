4baa131e5e301f6babee01d392f4e1aa34337bf4
JoshReilly <joshreilly2000@gmail.com>
2022-10-31 20:46:44 +0000

    final commit problem-2

--- lab2/problem-2/index.js
+++ lab2/problem-2/index.js
@@ -2,11 +2,26 @@
 var arr = [];
 fetch("https://jsonplaceholder.typicode.com/posts/")
   .then((response) => response.json())
-  .then((json) => json.map(myFunction));
-
-function myFunction(num) {
+  .then((json) => json.map(part1));
+//part 1
+function part1(num) {
   var words = num.title.split(" ");
   if (words.length > 6) {
     console.log(num.title);
   }
+  part2(num.body);
+}
+//part 2
+function part2(string) {
+  var pattern = /\w+/g,
+    matched = string.match(pattern);
+  var count = matched.reduce(function (stored, word) {
+    if (stored.hasOwnProperty(word)) {
+      stored[word] = stored[word] + 1;
+    } else {
+      stored[word] = 1;
+    }
+    return stored;
+  }, {});
+  console.log(count);
 }
