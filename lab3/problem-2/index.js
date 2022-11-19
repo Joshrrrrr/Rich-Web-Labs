08bf89303a4d73155d1db1c6723f1c47ab17ba91
JoshReilly <joshreilly2000@gmail.com>
2022-11-19 21:20:49 +0000

    Cleanup css and ui with placeholders and centering. Finish JS with alert on countdown reached and reload the page.

--- lab3/problem-2/index.js
+++ lab3/problem-2/index.js
@@ -2,8 +2,8 @@ var startbtn = document.getElementById("start");
 rxjs.fromEvent(startbtn, "click").subscribe(() => time());
 function start(time) {
   var timer = rxjs.timer(time * 1000);
-  // Update the count down every 1 second
-  var stoptime = new Date().getTime() + time * 1000;
+  // Update the count down every 1 second, I add one second on because it takes that long to start up and display
+  var stoptime = new Date().getTime() + time * 1000 + 1000;
   var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = stoptime - now;
@@ -14,14 +14,18 @@ function start(time) {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result
-    document.getElementById("display").innerHTML =
-      hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("display").innerHTML = hours + " : "+ minutes + " : "+ seconds;
 
     if (distance < 0) {
       clearInterval(x);
-      document.getElementById("display").innerHTML = "Refresh";
+      document.getElementById("display").innerHTML = "00 : 00";
     }
   }, 1000);
+  //Timer ends alert and refresh
+  timer.subscribe(function(){
+    alert("Times up!");
+    window.location.reload();
+  });
 }
 function time() {
   var h = document.getElementById("hours").value;
