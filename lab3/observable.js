d32a1a2e3f7d5b59560c6ca2fcbca254fab6957c
JoshReilly <joshreilly2000@gmail.com>
2022-11-27 12:47:19 +0000

    Add comments for previous commit

--- lab3/observable.js
+++ lab3/observable.js
@@ -46,29 +46,40 @@ function green() {
 // show elements from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
+  //note container div I made in html
   let notesContainer = document.getElementById("notes");
   if (notes == null) notesObj = [];
   else notesObj = JSON.parse(notes);
+  //remove all notes before we start so there are no duplicates by removing the lastchild of the container
   while (notesContainer.firstChild) {
     notesContainer.removeChild(notesContainer.lastChild);
   }
   // use internal css for the noteCards so changing the color and text is easy
   notesObj.forEach(function (element, index) {
+    //note div setting id, class and styling
     const note = document.createElement('div');
     note.setAttribute("id", "note-" + index);
     note.setAttribute("class", "noteCard");
     note.setAttribute("style",`"width:18rem;margin-right: 50px;margin-bottom: 50px; background-color:${color}"`);
+    //append to the note container div I made in html
     notesContainer.appendChild(note);
+    //Body of note setting class
     const notebody = document.createElement('div');
     notebody.setAttribute("class", "card-body");
+    //append to the note element above
     note.appendChild(notebody);
+    //Create note text element
     const p = document.createElement('p');
     p.setAttribute("class", "card-text");
+    //note text
     p.innerHTML=element;
+    //append text to the notebody element above
     notebody.appendChild(p);
+    //Create del button element
     const delBtn = document.createElement('button');
     delBtn.innerHTML = "Delete";
     delBtn.setAttribute("id", "del" + index);
+    //append button to the notebody element above
     notebody.appendChild(delBtn);
   });
 
