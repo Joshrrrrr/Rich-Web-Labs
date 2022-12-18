2a55e958dc20dbeb8f76a5cc2272dd08f09cd712
Joshrrrrr <C19303023@mytudublin.ie>
2022-12-18 01:04:54 +0000

    Declare notes array notesObj and include code from lab 1 notes app in add note handler. Also include showNotes function from lab 1.

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -2,17 +2,57 @@ import React, { useRef} from "react";
 function App() {
   const noteTextRef = useRef()
   var color = "white"
+  var notesObj = [];
 
   function handleAddNote(e){
     let addTxt = noteTextRef.current.value
     if(addTxt === '') return
-    console.log(addTxt)
     noteTextRef.current.value = null
+    let notes = localStorage.getItem("notes");
+    if (notes == null) notesObj = [];
+    else notesObj = JSON.parse(notes);
+
+    notesObj.push(addTxt);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    console.log(notesObj)
+    showNotes()
   }
 
   function handleColorNote(e){
     color = e.target.value
     console.log(color)
+    showNotes()
+  }
+
+  function showNotes() {
+    let notes = localStorage.getItem("notes");
+
+    if (notes == null){
+      let notesObj = [];
+    }
+    else notesObj = JSON.parse(notes);
+
+    let html = "";
+    // use internal css for the noteCards so changing the color and text is easy
+    notesObj.forEach(function (element, index) {
+      console.log(notes)
+      html += `<div class="noteCard"
+			style="width:18rem;margin-right: 50px;margin-bottom: 50px;
+			background-color:${color}">
+				<div class="card-body">
+					<p class="card-text">${element}</p>
+				<button id="${index}" onclick=
+					"deleteNote(this.id)">
+					Delete
+				</button>
+			</div>
+		</div>`;
+    });
+
+    let notesElm = document.getElementById("notes");
+    // check if there are any notes in notesObj
+    if (notesObj.length !== 0) notesElm.innerHTML = html;
+    else notesElm.innerHTML = "Nothing to show";
   }
   
   return (
