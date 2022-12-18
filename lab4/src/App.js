386df301c4c7b13c142b7c115ee8fce3700e4172
Joshrrrrr <C19303023@mytudublin.ie>
2022-12-18 00:59:52 +0000

    include html code from lab 1 notes app

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -1,6 +1,43 @@
 function App() {
   return (
-    null
+    <>
+    <div>
+		<div class="card">
+			<div class="card-body">
+				<h1 class="card-title">
+					Add a Note
+				</h1>
+				<div class="form-group">
+					<textarea class="form-control"
+						id="addTxt">
+					</textarea>
+				</div>
+				<button
+					id="addBtn">
+					Add Note
+				</button>
+				<button
+					id="blue" value="blue" onclick="blue()">
+					blue
+				</button>
+				<button
+					id="red" value="red" onclick="red()">
+					red
+				</button>
+				<button
+					id="green" value="green" onclick="green()">
+					green
+				</button>
+			</div>
+		</div>
+		<hr/>
+		<h1>Notes</h1>
+		<hr/>
+		<div id="notes" class=
+			"row container-fluid">
+		</div>
+	</div>
+  </>
   );
 }
 
